7fc2dec39870a31b443a68caf337dd345fc65798
Ojoc Diana-Cristiana <ojocdiana@gmail.com>
2024-12-08 18:10:53 +0200

    Footer

---
+++ Proiect-ip/Pages/DespreNoi.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proiect_ip.Pages
+{
+    public class DespreNoiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Proiect-ip/Pages/IntrebariFrecv.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proiect_ip.Pages
+{
+    public class IntrebariFrecvModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
