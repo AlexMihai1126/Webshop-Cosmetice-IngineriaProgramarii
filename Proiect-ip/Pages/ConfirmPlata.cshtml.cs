4cd1e8ee8d09f968a2e775c27f3ab27219f76ee4
Ojoc Diana-Cristiana <ojocdiana@gmail.com>
2024-12-09 18:34:21 +0200

    PCEC-29

---
+++ Proiect-ip/Pages/ConfirmPlata.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proiect_ip.Pages
+{
+    public class ConfirmPlataModel : PageModel
+
+    {
+        public string Status { get; set; } = "Fail"; // Default status
+
+        public void OnGet(string status)
+        {
+            Status = status; // Status passed as a query parameter (e.g., "Success" or "Fail")
+        }
+    }
+}
